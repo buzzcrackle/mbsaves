4226ceb68c7fe908a99fff1b07a59605f145cd91
unknown <jeliang4@697-334-Bree.sf.k12.internal>
2016-09-01 15:26:46 -0700

    things

--- troll/lel.js
+++ troll/lel.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$("div.percent").text("100%");
 	$("div.grade").text("A+");
 	$("div.zeros").text("Zeros: 0");
-	$("div.ajax_accordion_row").attr("data-url", "http://www.buzzcrackle.xyz/meme");
 
 	$("td.pr_link a").attr("href", "http://www.almao.xyz/")
 });
